b6b071034022218292bdd377298b75c7a17eb162
LotsOfBlochs <69881693+LotsOfBlochs@users.noreply.github.com>
2021-05-02 15:52:05 -0700

    Add project files.

---
+++ passwordChecker.cpp
@@ -0,0 +1,12 @@
+﻿// passwordChecker.cpp : Defines the entry point for the application.
+//
+
+#include "passwordChecker.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
